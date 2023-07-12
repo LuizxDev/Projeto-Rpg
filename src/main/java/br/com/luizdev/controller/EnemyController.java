30a90ca9683f9f43c0b030aa5adaf5bbef3bc01b
LuizxDev <luizxfelipe.dev@gmail.com>
2023-07-12 19:43:08 -0300

    pasta enemy criada

---
+++ src/main/java/br/com/luizdev/controller/EnemyController.java
@@ -0,0 +1,8 @@
+package br.com.luizdev.controller;
+
+import br.com.luizdev.model.enemys.EnemyModel;
+
+public class EnemyController extends EnemyModel {
+
+
+}
---
+++ src/main/java/br/com/luizdev/model/enemys/EnemyModel.java
@@ -0,0 +1,12 @@
+package br.com.luizdev.model.enemys;
+
+import br.com.luizdev.model.CharacterModel;
+
+public class EnemyModel extends CharacterModel {
+
+    /*public EnemyModel (String name, int damage){
+        this.setName(name);
+        this.setDamage(damage);
+    }*/
+    
+}
