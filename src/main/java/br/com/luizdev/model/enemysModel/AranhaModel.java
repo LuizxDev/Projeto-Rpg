9658e390d91a1249cb749d6c80b8b3505eddfcea
LuizxDev <luizxfelipe.dev@gmail.com>
2023-07-12 21:33:16 -0300

    poderes da aranha criado

--- src/main/java/br/com/luizdev/model/enemysModel/AranhaModel.java
+++ src/main/java/br/com/luizdev/model/enemysModel/AranhaModel.java
@@ -1,5 +1,7 @@
 package br.com.luizdev.model.enemysModel;
 
+import java.util.ArrayList;
+
 import br.com.luizdev.model.CharacterModel;
 
 
@@ -8,5 +10,19 @@ public class AranhaModel extends CharacterModel {
         this.setName("Aranha Venenosa");
         this.setLife(60);
         this.setDamage(25);
+    }
+
+    private int teiaPegajosa = 10; //valor fixo de 10
+    private int ataqueVenenoso = 20; //valor fixo
+
+    ArrayList<Integer> powers = new ArrayList<Integer>();
+
+    public void addPowers(){
+        powers.add(teiaPegajosa);
+        powers.add(ataqueVenenoso);
+    }
+
+    public ArrayList<Integer> getPowers(){
+        return powers;
     }
 }
